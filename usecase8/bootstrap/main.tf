605ac780b98d4ef81e78f6c8d4db1b8fd54c7834
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-07-22 17:17:32 +0530

    Update main.tf

--- usecase8/bootstrap/main.tf
+++ usecase8/bootstrap/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "appointment_repo" {
-  name = var.ecr_repo_name
+  name = var.ecr_appointment_repo
 }
 
 resource "aws_ecr_repository" "patient_repo" {
-  name = var.ecr_repo_name
+  name = var.ecr_patient_repo
 }
